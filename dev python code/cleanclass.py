6f7abcedd2b4adf381556aae88aff310000767e1
EstevaoVieira <estevovsky@gmail.com>
2017-05-04 15:06:14 -0300

    Add files via upload
    Files missing added by force
    get_features
    cleanclass

--- dev python code/cleanclass.py
+++ dev python code/cleanclass.py
@@ -23,6 +23,7 @@ from sklearn.metrics import roc_curve
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import fbeta_score, make_scorer
 from learning_curves import *
+import sys
 
 def sizeMatch(array):
     ash=array.shape
@@ -38,9 +39,9 @@ def norm_rgb2gray(image):
         if image.shape[2] == 3: return rgb2gray(image)
     except: return rgb2gray(image)*0.0721
 
-def AutoClassification(trainpercentage):
+def AutoClassification(trainpercentage, mode):
     #open images
-    directories=[r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/ClassAna",r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/Células classificadas - ZeH/Curva de crescimento/0dias/08-07-2016 (Menk)",r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/Células classificadas - ZeH/IM/16-09",r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/Células classificadas - ZeH/Sincronização jul2016/IM/31-08-2016 (Menck)", r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/SC_13-07-2016"]
+    directories=[r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/ClassAna",r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/Células classificadas - ZeH/Curva de crescimento/0dias/08-07-2016 (Menk)",r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/Células classificadas - ZeH/IM/16-09",r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/Células classificadas - ZeH/Sincronização jul2016/IM/31-08-2016 (Menck)"]
     #directories=[r"C:/Users/Estêvão/Documents/Scripts/Python Scripts/cellsData/ClassAna"]
 
     features = pd.DataFrame()
@@ -52,7 +53,6 @@ def AutoClassification(trainpercentage):
             features = pd.concat([features,df],ignore_index=True)
     features = features[np.logical_or(features['class']=='mitose'  ,features['class']=='interfase' )]
 
-    print(features)
 
     features["photorgb"] = features.file.apply(lambda x: misc.imread(x))
     features["photo"] = features.photorgb.apply(norm_rgb2gray)
@@ -60,21 +60,47 @@ def AutoClassification(trainpercentage):
 
     y = features['class'].values
     X_train,X_test,y_train,y_test = train_test_split(features,y, train_size=trainpercentage, stratify=y)
-    clf = cell_classifier(n_rotations=0)
-    print('Fitando o modelo')
-    clf.fit(X_train,y_train)
-    ypred = clf.predict(X_test)
+
+
     ####################### GRID SEARCH
-    scorer = make_scorer(lambda yt, yp: fbeta_score(yt, yp, beta=1,pos_label='mitose'))
+    scorer = make_scorer(lambda yt, yp: fbeta_score(yt, yp, beta=1.5,pos_label='mitose'))
 
-    #plot_learning_curve(clf, 'Learning Curves', X_train, y_train, cv=5, train_sizes=np.linspace(.1, 1.0, 2),scoring=scorer)
+    #this below is still on debug phase
+    clf = cell_classifier(min_samples=300, n_features=20,n_rotations=10)
 
-    yproba = clf.predict_proba(X_test)[:,1]
-    fpr,tpr, thr = roc_curve(y_test, yproba,pos_label='mitose')
-    plt.plot(fpr,tpr)
-    plt.show()
-    Ypredict= yproba>0.5; y_test= y_test=='mitose'
-    print(confusion_matrix(y_test, Ypredict))
-    print(np.mean(Ypredict==y_test))
+    #Tuning hyperparameters
+    #param_grid = {'min_samples':[2,5,10,25],'n_features':[5,10,15,20,25]}
+    #param_grid={'min_samples':[2,5]} #testing purposes
+    #clfgrid = GridSearchCV(clf, param_grid, scoring=scorer,cv=5)
+    #clfgrid.fit(X_train,y_train)
+    #print(pd.DataFrame(clfgrid.cv_results_))
+    #clf=clfgrid.best_estimator_
+    min_samples = 40
+    nfeat=20
+    if mode=='learningcurve':
+        print('Preparando curvas de aprendizado')
+        plot_learning_curve(clf, 'Samp'+str(min_samples)+'feat'+str(nfeat), X_train, y_train, cv=5, train_sizes=np.linspace(.5, 1, 5),scoring=scorer)
+        plt.show()
+        return
+    elif mode =='fit':
+        print('Fitando o modelo')
+        clf.fit(X_train,y_train)
+    elif mode =='roc':
+        print('Preparando curva ROC')
+        clf.fit(X_train,y_train)
+        yproba = clf.predict_proba(X_test)[:,1]
+        fpr,tpr, thr = roc_curve(y_test, yproba,pos_label='mitose')
+        plt.plot(fpr,tpr)
+        plt.xlabel("False Positives (1 - Precision)")
+        plt.ylabel("True Positives (Recall)")
+        plt.show()
+
+    tpred = clf.predict(X_train)
+    ypred = clf.predict(X_test)
+    #Ypredict= yproba>0.192; y_test= y_test=='mitose'
+    print(confusion_matrix(y_test, ypred))
+    print('Train Score : ' + str(fbeta_score(y_train, tpred, beta=1.5,pos_label='mitose')))
+    print('Test Score : '+ str(fbeta_score(y_test, ypred, beta=1.5,pos_label='mitose')))
+
 if __name__ == "__main__":
-    AutoClassification(.8)
+    AutoClassification(.8, sys.argv[1])
